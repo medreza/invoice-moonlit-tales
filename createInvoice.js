ea3f7078f273f7961f0167c8f8057e00eee3612d
Ahmed Reza Rafsanzani <31310263+medreza@users.noreply.github.com>
2024-12-29 16:37:54 +0700

    initial commit

---
+++ createInvoice.js
@@ -0,0 +1,218 @@
+const fs = require("fs");
+const PDFDocument = require("pdfkit");
+const path = require('path');
+const pdf = require("pdf-poppler");
+
+function createInvoice(invoice, pdfPath) {
+  let doc = new PDFDocument({ size: "A4", margin: 50 });
+
+  generateHeader(doc);
+  generateCustomerInformation(doc, invoice);
+  generateInvoiceTable(doc, invoice);
+  generateFooter(doc);
+
+  doc.end();
+  doc.pipe(fs.createWriteStream(pdfPath)).on('finish', () => {
+    const imageOutputDir = path.resolve(__dirname,'generated','images');
+    if (!fs.existsSync(imageOutputDir)) {
+      fs.mkdirSync(imageOutputDir);
+    }
+    convertPdfToImage(pdfPath, imageOutputDir);
+  });
+}
+
+function generateHeader(doc) {
+  doc
+    .image("logo-moonlit-tales.jpeg", 250, 35, { width: 100 })
+    .fillColor("#444444")
+    .fontSize(15)
+    .text("Moonlit Tales Books", 230, 125)
+    .fontSize(7)
+    .moveDown();
+}
+
+function generateCustomerInformation(doc, invoice) {
+  doc
+    .fillColor("#444444")
+    .fontSize(20)
+    .text("Invoice", 50, 160);
+
+  generateHr(doc, 185);
+
+  const customerInformationTop = 200;
+
+  doc
+    .fontSize(10)
+    .text("Invoice Number:", 50, customerInformationTop)
+    .font("Helvetica-Bold")
+    .text(invoice.invoice_nr, 150, customerInformationTop)
+    .font("Helvetica")
+    .text("Invoice Date:", 50, customerInformationTop + 15)
+    .text(formatDate(new Date()), 150, customerInformationTop + 15)
+    .text("Balance Due:", 50, customerInformationTop + 30)
+    .text(
+      formatCurrency(invoice.subtotal - invoice.paid),
+      150,
+      customerInformationTop + 30
+    )
+
+    .font("Helvetica-Bold")
+    .text(invoice.shipping.name, 300, customerInformationTop)
+    .font("Helvetica")
+    .text(invoice.shipping.phone, 300, customerInformationTop + 15)
+    .font("Helvetica-Bold")
+    .text(invoice.projectName, 300, customerInformationTop + 30)
+    .moveDown();
+
+  generateHr(doc, 252);
+}
+
+function generateInvoiceTable(doc, invoice) {
+  let i;
+  const invoiceTableTop = 330;
+
+  doc.font("Helvetica-Bold");
+  generateTableRow(
+    doc,
+    invoiceTableTop,
+    "Item",
+    "Description",
+    "Unit Cost",
+    "Quantity",
+    "Item Total"
+  );
+  generateHr(doc, invoiceTableTop + 20);
+  doc.font("Helvetica");
+
+  for (i = 0; i < invoice.items.length; i++) {
+    const item = invoice.items[i];
+    const position = invoiceTableTop + (i + 1) * 30;
+    generateTableRow(
+      doc,
+      position,
+      item.item,
+      item.description,
+      formatCurrency(item.amount / item.quantity),
+      item.quantity,
+      formatCurrency(item.amount)
+    );
+
+    generateHr(doc, position + 20);
+  }
+
+  const subtotalPosition = invoiceTableTop + (i + 1) * 30;
+  generateTableRow(
+    doc,
+    subtotalPosition,
+    "",
+    "",
+    "Subtotal",
+    "",
+    formatCurrency(invoice.subtotal)
+  );
+
+  const paidToDatePosition = subtotalPosition + 20;
+  generateTableRow(
+    doc,
+    paidToDatePosition,
+    "",
+    "",
+    "Paid To Date",
+    "",
+    formatCurrency(invoice.paid)
+  );
+
+  const duePosition = paidToDatePosition + 25;
+  doc.font("Helvetica-Bold");
+  generateTableRow(
+    doc,
+    duePosition,
+    "",
+    "",
+    "Balance Due",
+    "",
+    formatCurrency(invoice.subtotal - invoice.paid)
+  );
+  doc.font("Helvetica");
+}
+
+function generateFooter(doc) {
+  doc
+    .fontSize(10)
+    .text(
+      "Thank you for your purchase :)",
+      50,
+      770,
+      { align: "center", width: 500 }
+    )
+    .text(
+      "Follow us on Instagram @moonlittales.book",
+      50,
+      780,
+      { align: "center", width: 500 }
+    );
+}
+
+function generateTableRow(
+  doc,
+  y,
+  item,
+  description,
+  unitCost,
+  quantity,
+  lineTotal
+) {
+  doc
+    .fontSize(10)
+    .text(item, 50, y)
+    .text(description, 250, y)
+    .text(unitCost, 300, y, { width: 90, align: "right" })
+    .text(quantity, 370, y, { width: 90, align: "right" })
+    .text(lineTotal, 0, y, { align: "right" });
+}
+
+function generateHr(doc, y) {
+  doc
+    .strokeColor("#aaaaaa")
+    .lineWidth(1)
+    .moveTo(50, y)
+    .lineTo(550, y)
+    .stroke();
+}
+
+function formatCurrency(amount) {
+  return new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+    minimumFractionDigits: 0
+  }).format(amount);
+}
+
+function formatDate(date) {
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  return year + "/" + month + "/" + day;
+}
+
+function convertPdfToImage(pdfPath, outputDir) {
+  let opts = {
+    format: 'png',
+    out_dir: outputDir,
+    out_prefix: path.basename(pdfPath, path.extname(pdfPath)),
+    page: null
+  };
+
+  pdf.convert(pdfPath, opts)
+    .then(() => {
+      console.log(`Invoice image generated from ${pdfPath}`);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+module.exports = {
+  createInvoice
+};
---
+++ index.js
@@ -0,0 +1,42 @@
+const { file } = require("pdfkit");
+const { createInvoice } = require("./createInvoice.js");
+const dataForge = require('data-forge-fs');
+const path = require('path');
+
+const ProjectName = "BBW KL DES 2024"
+const InvoiceNumber = "2024/12/BBWKL";
+
+const BookTitleRow = "book title";
+const filePath = path.resolve(__dirname, 'data.csv');
+const dataFrame = dataForge.readFileSync(filePath).parseCSV();
+
+const groupedByBuyer = dataFrame.groupBy(row => row.buyer).toArray();
+
+var counter = 1;
+groupedByBuyer.forEach(group => {
+  const buyer = group.first().buyer;
+  const phone = group.first().phone;
+  const items = group.toArray().map(row => ({
+    item: row[BookTitleRow],
+    description: row.format,
+    quantity: row.quantity,
+    amount: row.price
+  }));
+
+  const subtotal = items.reduce((sum, item) => sum + parseFloat(item.amount), 0);
+
+  const invoice = {
+    shipping: {
+      name: buyer,
+      phone: phone,
+    },
+    items: items,
+    subtotal: subtotal,
+    paid: 0,
+    invoice_nr: `${InvoiceNumber}-${counter++}`,
+    projectName : ProjectName,
+  };
+
+  const invoicePath = path.resolve(__dirname, `generated/invoice__${invoice.invoice_nr.replace(/\//g, '_').replace(/\s+/g, '_')}_${buyer.replace(/\s+/g, '_')}.pdf`);
+  createInvoice(invoice, invoicePath);
+});
