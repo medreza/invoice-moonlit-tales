36b6e1e2ffd6604f67fb73c4129b316b310067d8
Ahmed Reza Rafsanzani <31310263+medreza@users.noreply.github.com>
2024-12-29 16:47:09 +0700

    chore: create generated dir if not exist

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-const { file } = require("pdfkit");
+const fs = require("fs");
 const { createInvoice } = require("./createInvoice.js");
-const dataForge = require('data-forge-fs');
-const path = require('path');
+const dataForge = require("data-forge-fs");
+const path = require("path");
 
-const ProjectName = "BBW KL DES 2024"
+const ProjectName = "BBW KL DES 2024";
 const InvoiceNumber = "2024/12/BBWKL";
 
 const BookTitleRow = "book title";
-const filePath = path.resolve(__dirname, 'data.csv');
-const dataFrame = dataForge.readFileSync(filePath).parseCSV();
 
-const groupedByBuyer = dataFrame.groupBy(row => row.buyer).toArray();
+const pdfOutputDir = path.resolve(__dirname, "generated");
+if (!fs.existsSync(pdfOutputDir)) {
+  fs.mkdirSync(pdfOutputDir);
+}
+
+const filePath = path.resolve(__dirname, "data.csv");
+const dataFrame = dataForge.readFileSync(filePath).parseCSV();
+const groupedByBuyer = dataFrame.groupBy((row) => row.buyer).toArray();
 
 var counter = 1;
-groupedByBuyer.forEach(group => {
+groupedByBuyer.forEach((group) => {
   const buyer = group.first().buyer;
   const phone = group.first().phone;
-  const items = group.toArray().map(row => ({
+  const items = group.toArray().map((row) => ({
     item: row[BookTitleRow],
     description: row.format,
     quantity: row.quantity,
-    amount: row.price
+    amount: row.price,
   }));
 
-  const subtotal = items.reduce((sum, item) => sum + parseFloat(item.amount), 0);
+  const subtotal = items.reduce(
+    (sum, item) => sum + parseFloat(item.amount),
+    0
+  );
 
   const invoice = {
     shipping: {
@@ -34,9 +42,14 @@ groupedByBuyer.forEach(group => {
     subtotal: subtotal,
     paid: 0,
     invoice_nr: `${InvoiceNumber}-${counter++}`,
-    projectName : ProjectName,
+    projectName: ProjectName,
   };
 
-  const invoicePath = path.resolve(__dirname, `generated/invoice__${invoice.invoice_nr.replace(/\//g, '_').replace(/\s+/g, '_')}_${buyer.replace(/\s+/g, '_')}.pdf`);
+  const invoicePath = path.resolve(
+    __dirname,
+    `generated/invoice_${invoice.invoice_nr
+      .replace(/\//g, "_")
+      .replace(/\s+/g, "_")}_${buyer.replace(/\s+/g, "_")}.pdf`
+  );
   createInvoice(invoice, invoicePath);
 });
